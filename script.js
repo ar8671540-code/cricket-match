44073e8b5250dfaadbfc83b6b7d8422846151965
ar8671540-code <ar8671540@gmail.com>
2026-02-04 22:32:25 +0530

    added index.html ,style.css and javascript.js

---
+++ script.js
@@ -0,0 +1,42 @@
+let runs = 0;
+let wickets = 0;
+let balls = 0;
+
+function addRun(value) {
+    runs += value;
+    balls++;
+    updateScore();
+}
+
+function addWicket() {
+    if (wickets < 10) {
+        wickets++;
+        balls++;
+        updateScore();
+    }
+}
+
+function reduceWicket() {
+    if (wickets > 0) {
+        wickets--;
+        updateScore();
+    }
+}
+
+function resetMatch() {
+    runs = 0;
+    wickets = 0;
+    balls = 0;
+    updateScore();
+}
+
+function updateScore() {
+    document.getElementById("runs").innerText = runs;
+    document.getElementById("wickets").innerText = wickets;
+
+    let overs = Math.floor(balls / 6) + "." + (balls % 6);
+    document.getElementById("overs").innerText = overs;
+}
+
+/* 📱 Extra: prevent double-tap zoom on mobile */
+document.addEventListener("touchstart", function () {}, true);
